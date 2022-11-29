6dbecbe53d10a187129d2495197b370a6317610c
Fortune <fortunejite1234@gmail.com>
2022-11-29 10:07:41 -0800

    ‘m’

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp, prev, new;
+
+	if (head == NULL || number == NULL)
+		return (NULL);
+	temp = *head;
+	new->n = number;
+	while (temp != NULL)
+	{
+		if (number > temp->n)
+		{
+			prev->next = new;
+			new->next = temp;
+		}
+		prev = temp;
+		temp = temp->next;
+	}
+	free_listint(temp);
+	free_listint(prev);
+	return (new);
+}
