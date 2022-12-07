171becf0842fc95a0cba01a1f4512cdccd2499f7
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-12-07 21:05:21 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     num = 0
     for i in a_dictionary:
-        if a_dictionary[i] == None:
+        if a_dictionary[i] is None:
             return None
         if a_dictionary[i] > num:
             num = a_dictionary[i]
