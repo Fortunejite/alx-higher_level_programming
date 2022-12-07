1d36a9772eba6f0e8f6e828c2fb35c4424c3d532
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2022-12-07 21:14:57 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    tmp = []
-    for i in my_list:
-        if i == 2:
-            tmp.append(89)
-        else:
-            tmp.append(i)
-    return tmp
+    if my_list is not None:
+        return([x if x != search else replace for x in my_list])
+    return None
