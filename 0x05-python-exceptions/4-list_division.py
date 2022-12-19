b07d3874df75ce40b66c90216d26ed059fbf5f37
Fortune <fortunejite1234@gmail.com>
2022-12-19 05:08:42 -0800

    ‘m’

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    idx = 0
+    while idx < list_lenght:
+        
+        new.append(my_list_1[idx] / my_list_2[idx])
