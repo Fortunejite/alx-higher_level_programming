14b099195ec6911ca08cfa4502985f15b02a990f
Fortune <fortunejite1234@gmail.com>
2022-12-19 11:30:26 -0800

    ‘m’

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
+from sys import stderr
+
+def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as err:
-        print("Exception: {}".format(err))
+        stderr.write("Exception: {}\n".format(err))
         return False
