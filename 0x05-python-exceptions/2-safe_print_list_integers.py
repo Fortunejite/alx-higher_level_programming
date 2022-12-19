e96599c5249b68e61ff841633f9f66d09029a643
Fortune <fortunejite1234@gmail.com>
2022-12-19 06:33:01 -0800

    ‘m’

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-        idx = 0
-        count = 0
-        while idx < x:
-            try:
-                print("{:d}".format(my_list[idx]), end="")
-                idx += 1
-                count += 1
-            except (TypeError, ValueError):
-                idx += 1
-                continue
-        print()
-        return count
-            
-
+    idx = 0
+    count = 0
+    while idx < x:
+        try:
+            print("{:d}".format(my_list[idx]), end="")
+            idx += 1
+            count += 1
+        except (TypeError, ValueError):
+            idx += 1
+            continue
+    print()
+    return count
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -20,5 +20,4 @@ def list_division(my_list_1, my_list_2, list_length):
             idx += 1
         finally:
             new.append(ans)
-    return new
-            
+    return new 
