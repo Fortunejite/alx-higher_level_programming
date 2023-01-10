8577bd52fb1a7bb209237d4d82d576fb53322109
Fortune <fortunejite1234@gmail.com>
2023-01-10 07:33:36 -0800

    ‘m’

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,6 @@
 def save_to_json_file(my_obj, filename):
     """Cinverts object to string"""
     import json
-    json_str = json.dumps(my_obj)
+    
     with open(filename, 'w', encoding='utf-8') as f:
-        return f.write(json_str)
+        return f.write(json.dumps(my_obj))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,8 +5,8 @@
 def load_from_json_file(filename):
     """Cinverts object to string"""
     import json
-    
+
     with open(filename, 'r', encoding='utf-8') as f:
-        
+
         json_str = f.read()
         return json.loads(json_str)
