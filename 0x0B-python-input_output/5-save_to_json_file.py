257b1990714640da9fdc17adb0e893282cb7af28
Fortune <fortunejite1234@gmail.com>
2023-01-10 09:16:10 -0800

    ‘m’

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,6 +5,6 @@
 def save_to_json_file(my_obj, filename):
     """Cinverts object to string"""
     import json
-    
+
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(json.dumps(my_obj))
---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Getting mir complex with JSON"""
+
+
+import sys
+save = __import__('5-save_to_json_file').save_to_json_file
+load = __import__('6-load_from_json_file').load_from_json_file
+
+args = []
+for i in sys.argv:
+    args.append(i)
+args = args[1:]
+save(args, 'add_item.json')
+load('add_item.json')
