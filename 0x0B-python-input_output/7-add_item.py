c642e8fb868ec3df907fb7639d848a6361c118e4
Fortune <fortunejite1234@gmail.com>
2023-01-10 09:36:40 -0800

    ‘m’

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,9 +6,13 @@ import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-args = []
+try:
+    args = load('add_item.json')
+except:
+    args = []
 for i in sys.argv:
     args.append(i)
 args = args[1:]
-save(args, 'add_item.json')
-load('add_item.json')
+try:
+    save(args, 'add_iteexcept:
+            pass
