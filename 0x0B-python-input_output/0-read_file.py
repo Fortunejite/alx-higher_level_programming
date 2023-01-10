0a0f27e5f52ff16c540acfd4267d832afdc68fd6
Fortune <fortunejite1234@gmail.com>
2023-01-10 05:14:32 -0800

    ‘m’

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Morule they reads a given file"""
 
+
 def read_file(filename=""):
     """prints the content of filename to stdoit"""
     with open(filename, encoding="utf-8") as f:
