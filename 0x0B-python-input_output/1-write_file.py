eba2563e9550fb5e66d3d5caa27a0ced5fd72bf6
Fortune <fortunejite1234@gmail.com>
2023-01-10 05:19:24 -0800

    ‘m’

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Morule they reads a given file"""
 
+
 def write_file(filename="", text=""):
     """prints the content of filename to stdoit"""
     with open(filename, 'w', encoding='utf-8') as file_:
