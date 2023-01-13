8aa6258705a87948188f5275c7302eefedac129b
Fortune <fortunejite1234@gmail.com>
2023-01-13 02:24:34 -0800

    ‘m’

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,41 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = self.width
+
+    def update(self, *args, **kwargs):
+        """update the attribute"""
+
+        if len(args) != 0:
+            arg = list(args)
+            for i in range(len(arg)):
+                if i == 0:
+                    self.id = arg[0]
+                elif i == 1:
+                    self.size = arg[1]
+                elif i == 2:
+                    self.x = arg[2]
+                elif i == 3:
+                    self.y = arg[3]
+                else:
+                    continue
+        else:
+            for i in kwargs:
+                if i == "id":
+                    self.id = kwargs[i]
+                elif i == "size":
+                    self.width = kwargs[i]
+                elif i == "x":
+                    self.x = kwargs[i]
+                elif i == "y":
+                    self.y = kwargs[i]
+                else:
+                    continue;
+
+    def to_dictionary(self):
+        dic = {}
+        dic['id'] = self.id
+        dic['width'] = self.width
+        dic['height'] = self.size
+        dic['x'] = self.x
+        dic['y'] = self.y
+        return dic
