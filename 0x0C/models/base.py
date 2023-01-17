4c94737f643dbd26cc3ca492fe310b618b09dd49
Fortune <fortunejite1234@gmail.com>
2023-01-16 20:35:58 -0800

    ‘m’

---
+++ 0x0C/models/base.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python3
+"""The base module for this project.
+"""
+
+
+class Base:
+    """The base class for this project
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts.
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        import json
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        lists = []
+        string = ""
+        name = cls.__name__ + ".json"
+        if list_objs is not None:
+            with open(name, 'w', encoding='utf-8') as f:
+                for i in list_objs:
+                    lists.append(i.to_dictionary())
+                string = Base.to_json_string(lists)
+                f.write(string)
+        else:
+            with open(name, 'w', encoding='utf-8') as f:
+                string = Base.to_json_string(lists)
+                f.write(string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        import json
+        if json_string is None or json_string == '':
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        dummy = cls(3, 3)
+        for i in dictionary:
+            if i == "id":
+                dummy.update(id=dictionary[i])
+            elif i == "size":
+                dummy.update(size=dictionary[i])
+            elif i == "x":
+                dummy.update(x=dictionary[i])
+            elif i == "y":
+                dummy.update(y=dictionary[i])
+            elif i == 'height':
+                dummy.update(height=dictionary[i])
+            elif i == 'width':
+                dummy.update(width=dictionary[i])
+            else:
+                continue
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        name = cls.__name__ + '.json'
+        list_ = []
+        try:
+            with open(name, 'r') as f:
+                string = f.read()
+            string = Base.from_json_string(string)
+            for i in string:
+                r = cls.create(**i)
+                list_.append(r)
+            return list_
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        name = cls.__name__ + ".csv"
+        header = ['id', 'width', 'height', 'x', 'y']
+        rows = []
+        import csv
+        with open(name, 'w', encoding='utf-8') as f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerow(header)
+            if list_objs is None:
+                pass
+            elif cls.__name__ == 'Rectangle':
+                for i in list_objs:
+                    rows = []
+                    rows.append(i.id)
+                    rows.append(i.width)
+                    rows.append(i.height)
+                    rows.append(i.x)
+                    rows.append(i.y)
+                    csvwriter.writerow(rows)
+            elif cls.__name__ == 'Square':
+                for i in list_objs:
+                    rows = []
+                    rows.append(i.id)
+                    rows.append(i.size)
+                    rows.append(i.x)
+                    rows.append(i.y)
+                    csvwriter.writerow(rows)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        import csv
+        name = cls.__name__ + '.csv'
+        with open(name, 'r') as f:
+            csvr = csv.reader(f)
+            rows = []
+            next(csvr)
+            for i in csvr:
+                if cls.__name__ == "Rectangle":
+                    width = int(i[1])
+                    r = cls(width, int(i[2]), int(i[3]), int(i[4]))
+                    r.id = int(i[0])
+                elif cls.__name__ == "Square":
+                    r = cls(int(i[1]), int(i[2]), int(i[3]))
+                    r.id = int(i[0])
+                rows.append(r)
+            return rows
---
+++ 0x0C/models/rectangle.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+""" Module that will inherit"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Representation of a rectangle that inherits from base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = width
+
+        if type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = height
+
+        if type(x) != int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = x
+
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
+    def area(self):
+        """Return the area of the rectangle class"""
+        return self.height * self.width
+
+    def display(self):
+        """prints to stdout the rectangle imstance"""
+        for i in range(self.y):
+            print()
+        for i in range(self.height):
+            print(" " * self.x, end="")
+            print("#" * self.width)
+
+    def __str__(self):
+        """Change the representation of the instance"""
+        n = "Rectangle"
+        d = self.id
+        return f"[{n}] ({d}) {self.x}/{self.y} - {self.width}/{self.height}"
+
+    def update(self, *args, **kwargs):
+        """update the attribute"""
+
+        if len(args) != 0:
+            arg = list(args)
+            for i in range(len(arg)):
+                if i == 0:
+                    self.id = arg[0]
+                elif i == 1:
+                    self.width = arg[1]
+                elif i == 2:
+                    self.height = arg[2]
+                elif i == 3:
+                    self.x = arg[3]
+                elif i == 4:
+                    self.y = args[4]
+                else:
+                    continue
+        else:
+            for i in kwargs:
+                if i == "id":
+                    self.id = kwargs[i]
+                elif i == "width":
+                    self.width = kwargs[i]
+                elif i == "height":
+                    self.height = kwargs[i]
+                elif i == "x":
+                    self.x = kwargs[i]
+                elif i == "y":
+                    self.y = kwargs[i]
+                else:
+                    continue
+
+    def to_dictionary(self):
+        dic = {}
+        dic['x'] = self.x
+        dic['y'] = self.y
+        dic['id'] = self.id
+        dic['height'] = self.height
+        dic['width'] = self.width
+        return dic
