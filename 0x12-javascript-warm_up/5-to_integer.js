9fcaff67027b34edb1e8e000d12e96d819350c08
Fortune <fortunejite1234@gmail.com>
2023-03-13 13:17:34 +0000

    j

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 const arg = process.argv;
-if (Number.isNaN(parseInt(arg[2]))) {
+if (isNaN(arg[2])) {
   console.log('Not a number');
 } else {
   console.log(parseInt(arg[2]));
