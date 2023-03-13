a1c8fc7c03bef85e7d6ad54f20118bb4c21c9cc2
Fortune <fortunejite1234@gmail.com>
2023-03-13 18:35:40 +0000

    n

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,13 +4,13 @@ const arg = process.argv;
 if (arg.length < 3) {
   console.log(0);
 } else if (arg.length === 3) {
- console.log(0);
+  console.log(0);
 } else {
   for (let i = 2; i < arg.length; i++) {
     arg[i] = parseInt(arg[i]);
   }
   arg.shift();
   arg.shift();
-  arg.sort(function(a, b){return a - b});
+  arg.sort(function (a, b) { return a - b; });
   console.log(arg[arg.length - 2]);
 }
