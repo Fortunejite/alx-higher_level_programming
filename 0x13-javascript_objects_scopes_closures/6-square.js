7ebf01f546efb6fea46622b85c391b9892cc65a1
Fortune <fortunejite1234@gmail.com>
2023-03-14 12:30:21 +0000

    h

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    if (c === undefined) {
+      for (let i = 0; i < this.size; i++) {
+        console.log('X'.repeat(this.size));
+      }
+    } else {
+      for (let i = 0; i < this.size; i++) {
+        console.log(c.repeat(this.size));
+      }
+    }
+  }
+};
