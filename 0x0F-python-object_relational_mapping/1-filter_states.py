b0533b5475cca9a16198bdcd2df0cf9392e5831d
Fortune <fortunejite1234@gmail.com>
2023-03-17 00:45:56 -0700

    m

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
-"""
- lists all states from the database hbtn_0e_0_usa
- using mysqldb  and mysql as the sql
-"""
+""" gets all states via python yee boi"""
+
+
+def main(args):
+    """ gets all state stuff"""
+    if len(args) != 4:
+        raise Exception("need 3 arguments!")
+    db = MySQLdb.connect(host='localhost',
+                         user=args[1],
+                         passwd=args[2],
+                         db=args[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        port=3309,
-        db=sys.argv[3]
-    )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
-    db.close()
+    import sys
+    import MySQLdb
+    main(sys.argv)
