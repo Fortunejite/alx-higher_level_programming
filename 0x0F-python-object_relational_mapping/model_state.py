4e805f3539acc8acf3e8269ed32524e1e8d7917b
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-03-18 10:31:22 +0100

    M

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
+from SQLAlchemy.orm import declarative_base
+from SQLAlchemy import Column, String, Integer
+
 """Using sqlqlchemy ti create sql tables to python class """
 
 
-def main():
-    """The main function for the while operation"""
-    Base = declarative_base()
-    class State(Base):
-        __tablename__ = "states"
+Base = declarative_base()
+class State(Base):
+    __tablename__ = "states"
 
-        id = Column(Integer(), nullable=False, unique=True, primary_key=True)
-        name = Column(String(128), nullable=False, unique=True)
-
-if __name__ == "__main__":
-    from SQLAlchemy.orm import declarative_base
-    from SQLAlchemy import Column, String, Integer
-    main()
+    id = Column(Integer(), nullable=False, unique=True, primary_key=True)
+    name = Column(String(128), nullable=False, unique=True)
