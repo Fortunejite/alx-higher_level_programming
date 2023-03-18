bce1f2506d3cc9c4289413a717554b2609f2e6f2
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-03-18 10:41:58 +0100

    M

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 
 """Using sqlqlchemy ti create sql tables to python class """
@@ -9,7 +9,8 @@ Base = declarative_base()
 
 
 class State(Base):
+    """The model of the state class."""
     __tablename__ = "states"
 
-    id = Column(Integer(), nullable=False, unique=True, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False, unique=True)
