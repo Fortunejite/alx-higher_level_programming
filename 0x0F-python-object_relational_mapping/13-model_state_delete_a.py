fdc06390d1d1fff987437650879fd3c8b5e02637
Fortune <fortunejite1234@gmail.com>
2023-03-18 05:20:12 -0700

    m

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that returns all the ibject in the database"""
+
+
+def main(arg):
+    """The main function where all the codes are written """
+    if len(arg) != 4:
+        raise Exception("need 3 arguments!")
+    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(arg[1], arg[2], arg[3])
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new = session.query(State).filter(State.id == 2).first()
+    new.name = "New Mexico"
+    session.add(new)
+    session.commit()
+    session.close()
+
+
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import MetaData
+    main(argv)
