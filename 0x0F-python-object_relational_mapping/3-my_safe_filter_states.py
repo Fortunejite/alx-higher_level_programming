03e3ea103a306efd426b1f350164ab626bf9838e
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-03-19 20:13:11 +0100

    M

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
+"""
+Lists all values in the states tables of a database where name
+matches the argument in a safe way
+"""
 import sys
 import MySQLdb
-"""
- lists all states from the database hbtn_0e_0_usa
- using mysqldb  and mysql as the sql
-"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        port=3309,
-        db=sys.argv[3]
-    )
-    cursor = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s;", (sys.argv[4],))
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
-    db.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
