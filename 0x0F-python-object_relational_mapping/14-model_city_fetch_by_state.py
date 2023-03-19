def8082ec683b47bee03807b4a95d48ed4cf1e36
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-03-19 20:23:04 +0100

    M

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""Module that returns all the ibject in the database"""
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
-def main(arg):
-    """The main function where all the codes are written """
-    if len(arg) != 4:
-        raise Exception("need 3 arguments!")
-    url = 'mysql+mysqldb://{}:{}@localhost/{}'.format(arg[1], arg[2], arg[3])
-    engine = create_engine(url, pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for city, state in session.query(City, State) \
-                              .filter(City.state_id == State.id) \
-                              .order_by(City.id):
-        print('{}: ({}) {}'.format(state.name, city.id, city.name))
-    session.close()
 
+    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
 
-if __name__ == "__main__":
-    from model_state import Base, State
-    from model_city import City
-    from sys import argv
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy import MetaData
-    main(argv)
+    for state, city in st_cty:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
