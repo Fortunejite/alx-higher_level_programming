ac5582731a20be51623d4608c44150c23511f04f
Fortunejite <111015282+Fortunejite@users.noreply.github.com>
2023-03-31 23:19:39 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 #curls to end
-curl -sL "$1"
+if [ -z "$1" ]; then
+  echo "Usage: $0 <url>"
+  exit 1
+fi
+
+# Send GET request to the URL using curl
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+
+# Check if the status code is 200
+if [ "$response" -eq "200" ]; then
+  # Display the body of the response
+  curl -s "$1"
+else
+  echo "Error: HTTP status code $response"
+fi
