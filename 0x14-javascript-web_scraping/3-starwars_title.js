332c6b983ed65b0ae23b0c722999e2b22eacc1e8
Your Name <you@example.com>
2023-04-27 01:26:56 +0100

    m

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let request = require('request');
-let num = process.argv[2];
+const request = require('request');
+const num = process.argv[2];
 request('http://swapi.co/api/films/' + num, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    console.log(JSON.parse(body)['title']);
+    console.log(JSON.parse(body).title);
   }
 });
